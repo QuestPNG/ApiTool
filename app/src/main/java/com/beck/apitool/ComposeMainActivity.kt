4d89cfd879eb5ff9ce0c50c35ce0127001d38526
Devin <devin24beck@gmail.com>
2023-12-07 11:44:23 -0500

    Merged Gus into Main and changed MainActivity to toggle between HTTP and WS screens

--- app/src/main/java/com/beck/apitool/ComposeMainActivity.kt
+++ app/src/main/java/com/beck/apitool/ComposeMainActivity.kt
@@ -3,9 +3,16 @@ package com.beck.apitool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.beck.apitool.ui.screens.HttpScreen
 import com.beck.apitool.ui.screens.WsScreen
@@ -21,8 +28,21 @@ class ComposeMainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //HttpScreen()
-                    WsScreen()
+                    var showWS by remember { mutableStateOf(false) }
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Switch(
+                            checked = showWS,
+                            onCheckedChange = { showWS = !showWS }
+                        )
+                        if (showWS) {
+                            WsScreen()
+                        } else {
+                            HttpScreen()
+                        }
+                    }
                 }
             }
         }
